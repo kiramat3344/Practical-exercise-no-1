ef424dad8f2b5b9cbb176c27f3d51ce8a0380d71
kiramat3344 <kiramat0323@gmail.com>
2024-09-13 11:00:39 +0500

    this is new file

---
+++ script.js
@@ -0,0 +1,10 @@
+// GRADE CALCULATOT
+
+let marks = prompt("Enter your marks (0-100)");
+if(marks > = 0 && marks > = 100){
+
+}
+else{
+    alert("Invalid input. Please enter a number between 0 and 100.");
+    return;
+}
